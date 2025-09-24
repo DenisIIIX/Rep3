f451f4c12ee0954f9d4b8535c50c25b9ef0b9e4f
денис волков <vdv.duf@mail.ru>
2025-09-25 01:57:33 +0300

    fixMerge pull request #1 from DenisIIIX/Lab2
    fix

---
+++ Example2Task1/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Example2Task1
+{
+    internal class Program
+    {
+        public class NotificationService
+        {
+            private readonly INotificationSender _notificationSender;
+            private readonly ILogger _logger;
+
+            public NotificationService(INotificationSender notificationSender, ILogger logger)
+            {
+                _notificationSender = notificationSender; 
+                _logger = logger;
+            }
+
+            public void SendNotification(string message, string recipient)
+            {
+                
+                string formattedMessage = $"Уведомление: {message}";
+
+                _notificationSender.Send(recipient, formattedMessage);
+                
+                _logger.Log($"Отправлено уведомление для {recipient}");
+            }
+        }
+
+        class Program1
+        {
+            static void Main()
+            {
+                
+                ServiceCollection services = new ServiceCollection();
+                services.AddSingleton<ILogger,FileLogger>();
+                services.AddSingleton<EmailSender>();
+                services.AddSingleton<SmsSender>();
+
+                ServiceProvider provider = services.BuildServiceProvider();
+
+                Console.WriteLine("Выберите тип уведомления:");
+                Console.WriteLine("1 - Email");
+                Console.WriteLine("2 - SMS");
+                Console.Write("Ваш выбор: ");
+                var choise = Console.ReadLine();
+                INotificationSender sender = choise switch
+                { 
+                    "1" => provider.GetRequiredService<EmailSender>(),
+                    "2"=> provider.GetRequiredService<SmsSender>(),                    
+                };
+                var logger = provider.GetRequiredService<ILogger>();
+                
+                var service = new NotificationService(sender,logger);
+                service.SendNotification("Ваш заказ готов", "user@example.com");
+
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }       
+    }
+}
---
+++ Example2Task1/Sender.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Example2Task1
+{
+    public interface INotificationSender
+    {
+        void Send(string to, string message);
+    }
+    public class EmailSender : INotificationSender
+    {
+
+        public void Send(string to, string message)
+        {
+            // Симуляция отправки email
+            Console.WriteLine($"Email для {to}: {message}");
+        }
+    }
+    public class SmsSender : INotificationSender
+    {
+
+        public void Send(string to, string message)
+        {
+            // Симуляция отправки email
+            Console.WriteLine($"Sms для {to}: {message}");
+        }
+    }
+}
